621f4f54886a046d82ba305425661cfe3494b612
mnltsbzv <80626293+mnltsbzv@users.noreply.github.com>
2021-05-10 14:19:31 +0300

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,6 @@
+let a = 'Я' ;
+let b = 'изучаю' ;
+let c = 'javaScript';
+alert (a);
+alert (b);
+alert (c);
---
+++ script3.js
@@ -0,0 +1,3 @@
+alert( isInteger(1) ); // true
+alert( isInteger(1.5) ); // false
+alert( isInteger(­0.5) ); // false
